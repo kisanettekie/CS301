f778f3624e11a188dd19cb71e89cd26011405b7a
kisanettekie <ktekie@miu.edu>
2021-02-22 16:00:54 -0600

    classDemoQuestionDone

---
+++ w1d1Assignment/demoClass.js
@@ -0,0 +1,25 @@
+//question 1
+
+console.log("5+5");
+
+console.log("------------------");
+
+//question 2
+
+const X=7;
+const Y=3;
+
+console.log(X+Y);
+
+console.log("------------------");
+
+//question 3
+
+let x=4;
+let y=5;
+
+console.log(x+y);
+console.log("------------------");
+
+console.log("Hello World");
+alert("Hello World");
