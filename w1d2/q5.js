1a16c9f3d8218512564157a606fa45eb88e32af7
kisanettekie <ktekie@miu.edu>
2021-03-02 22:30:09 -0600

    updatedVersion

---
+++ w1d2/q5.js
@@ -0,0 +1,22 @@
+function getGHF(num1,num2){
+    let min=Math.min(num1, num2)
+    let gcf=1;
+    for(let i=2;i<=min;i++){
+    if(num1%i===0 && num2%i===0){
+    gcf=i
+    }
+    }
+    return gcf;
+   }
+   console.log(getGHF(100,230));
+
+
+   let lcm= function(num1,num2){
+   let gcd=getGHF(num1,num2)
+
+   let product= num1*num2;
+    return product/gcd;
+
+   }
+
+   console.log(lcm(100,230));
---
+++ w1d2/q7.js
@@ -0,0 +1,28 @@
+// q7
+
+//let compute= function(add, a,b){
+
+// return add(a,b);
+
+
+    
+// }
+// let add= function(a,b){
+
+//     return a+b;
+
+// }
+// console.log(compute(add,2,3));
+
+
+
+
+
+//-------------//
+
+// q7 Arrow
+let compute= (add, a,b)=> add(a,b);
+  
+let add= (a,b)=> a+b;
+    
+    console.log(compute(add,2,3));
