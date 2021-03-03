a423efbcd3ecdea5ad5b4ec94cd80c10bf563abf
kisanettekie <ktekie@miu.edu>
2021-03-02 21:15:54 -0600

    w1d2 Function Expression

---
+++ w1d2/q4.js
@@ -0,0 +1,29 @@
+function getGHF1(num1,num2){
+    let min=Math.min(num1, num2)
+    let gcf=1;
+    for(let i=2;i<=min;i++){
+    if(num1%i===0 && num2%i===0){
+    gcf=i
+    }
+    }
+    return gcf;
+   }
+   console.log(getGHF1(12,24))
+
+
+   ////Short cut to find GCF
+   function getNewGCF(a,b){
+    if(a<b){
+    let temp=a;
+    a=b;
+    b=temp;
+    }
+    do{
+    let r=a%b;
+    a=b;
+    b=r
+    }while(b!==0)
+    return a;
+   }
+   console.log(getNewGCF(24,12))
+
