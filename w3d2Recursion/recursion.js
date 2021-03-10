c65e7813a834f5c46fa2233b9bdb7fc300d39d2d
kisanettekie <ktekie@miu.edu>
2021-03-09 18:46:20 -0600

    Updated

---
+++ w3d2Recursion/recursion.js
@@ -0,0 +1,38 @@
+// A
+
+function pow(a,n){
+    if(n === 0){
+        return 1;
+    }else{
+        return a * pow (a,n-1);
+    }
+}
+console.log(pow(5,2));
+
+// B
+
+function digit(n){
+    if(n === 0){
+        return 0;
+    }else{
+        return 1 + digit(parseInt(n/10));
+    }
+}
+console.log(digit(23456));
+
+
+
+// C
+
+function reverse(str){
+    if(str === ""){
+        return "";
+    }else{
+        let result= str.substring(str.length-1);
+        return result + reverse(str.substring(0,str.length-1));
+    }
+}
+console.log(reverse("kisanet"));
+
+
+module.exports={pow,digit,reverse};
